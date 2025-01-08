7dd16c9a0c8bdac9d9c21931dba5e11ecf6e103f
Jagman <Jagmansidhu1@icloud.com>
2025-01-07 19:38:11 -0800

    added keyhandler and moving box around screen while updating location correctly

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -16,7 +16,15 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    final int fps = 60;
+
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread; //keeps program running till i stop
+
+    //players default positon
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
@@ -24,10 +32,12 @@ public class GamePanel extends JPanel implements Runnable {
         this.setBackground(Color.BLACK);
         // improves gahmes rendering performance
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true); // gamepanel focused to receive input
 
     }
 
-    public void startGameThread(){
+    public void startGameThread() {
 
         gameThread = new Thread(this);
         gameThread.start();
@@ -36,22 +46,53 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = 1000000000 / fps;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         //create game loop core of game
-        while(gameThread.isAlive()){
+        while (gameThread.isAlive()) {
 
             // Update Game information
             update();
 
             // Draw Screen with updated information
             repaint(); // this is how you call paint component
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
-    public void update(){
 
+    public void update() {
+        if (keyHandler.upPressed) {
+            playerY -= playerSpeed;
+        }
+        if (keyHandler.downPressed) {
+            playerY += playerSpeed;
+        }
+        if (keyHandler.leftPressed) {
+            playerX -= playerSpeed;
+        }
+        if (keyHandler.rightPressed) {
+            playerX += playerSpeed;
+        }
     }
 
     //use this to paint onto scree Graphics is our paintbrush
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
 
         super.paintComponent(g);
 
@@ -59,7 +100,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         g2.setColor(Color.WHITE);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         //once drawing above done we dispose to save memory
         g2.dispose();
     }
